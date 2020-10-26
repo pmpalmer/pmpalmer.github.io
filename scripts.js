ecb23fadbd32efa20e84a89a89cc89b10194ec50
Paul Palmer <pmpalmer8@gmail.com>
2020-10-26 19:12:35 -0400

    page setup

---
+++ scripts.js
@@ -0,0 +1,20 @@
+window.onload = function() {
+	var buttons = Array.from(document.getElementsByClassName('button'));
+	var content = document.getElementById('content');
+	var contact = document.getElementById('contact');
+	
+	buttons.forEach( function(button) {
+		button.onclick = function() {
+		    var div = document.getElementById('contact');
+		    if (!div.classList.contains('active')) {
+		    	div.classList.add('active');
+		    	content.setAttribute('aria-hidden', 'true');
+		    	contact.setAttribute('aria-hidden', 'false');
+		    } else {
+		    	div.classList.remove('active');
+		    	content.setAttribute('aria-hidden', 'false');
+		    	contact.setAttribute('aria-hidden', 'true');
+		    }
+		};
+	});
+};
